192de22ec358b94d70e734719ca9ecfbad9012cd
Levi Kohler <kohler.la01@gmail.com>
2021-01-29 11:09:04 -0800

    add additional forms to HTML as well as add additional property to constructor and protptype

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 // Business Logic
-function Pizza(size, cheese, topping) {
+function Pizza(size, cheese, meat, veggie) {
   this.size = size
   this.cheese = cheese
-  this.topping = topping
+  this.meat = meat
+  this.veggie
 }
 
 Pizza.prototype.totalPrice = function() {
-  let sumPrice = (this.size = this.cheese = this.topping)
+  let sumPrice = (this.size + this.cheese + this.meat + this.veggie)
   return sumPrice
 }
 
