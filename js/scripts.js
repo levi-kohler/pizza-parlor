b710787bb444c7f01edf6d5686d7e4125523410f
Levi Kohler <kohler.la01@gmail.com>
2021-01-29 11:57:30 -0800

    add submit button as well as UI logic to calculate price, current bug where price only shows as NaN

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,15 @@ Pizza.prototype.totalPrice = function() {
 }
 
 // UI Logic
+$(document).ready(function() {
+  $("form#pizzaForm").submit(function(e) {
+    e.preventDefault();
+    let size = parseInt($("#sizePrice").val());
+    let cheese = parseInt($("#cheesePrice").val());
+    let meat = parseInt($("#meatPrice").val());
+    let veggie = parseInt($("#veggiePrice").val());
+    let pizzaPrice = new Pizza(size,cheese,meat,veggie);
+    $("#show-price").show
+    $("#show-price").text(pizzaPrice.totalPrice());
+  })
+})
